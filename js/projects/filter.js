068c9dc265ce71067f613d7b57cd4e817e8f8dac
Olamide George <73113006+OLABOSS123@users.noreply.github.com>
2026-08-18 23:24:16 +0100

    update filter,js
    Added functionality to filter project buttons based on selected filter.

--- js/projects/filter.js
+++ js/projects/filter.js
@@ -1,52 +1,66 @@
-/*==========================================================
-                    PROJECT FILTER
-==========================================================*/
-
 const filterButtons = document.querySelectorAll(".filter-btn");
-
 const projectCards = document.querySelectorAll(".project-card");
+const projectButtons = document.querySelectorAll(".project-buttons");
 
 filterButtons.forEach(button => {
 
     button.addEventListener("click", () => {
 
-        filterButtons.forEach(btn =>
-            btn.classList.remove("active")
-        );
+        filterButtons.forEach(btn => {
+            btn.classList.remove("active");
+        });
 
         button.classList.add("active");
 
         const filter = button.dataset.filter;
 
+
+        /* ==========================================
+           PROJECT ARTICLES
+        ========================================== */
+
         projectCards.forEach(card => {
 
             const category = card.dataset.category;
 
-            if(filter === "all" || category === filter){
+            if (filter === "all" || category === filter) {
 
                 card.style.display = "flex";
 
                 setTimeout(() => {
-
                     card.style.opacity = "1";
-
                     card.style.transform = "scale(1)";
+                }, 100);
 
-                },150);
-
-            }
-
-            else{
+            } else {
 
                 card.style.opacity = "0";
-
                 card.style.transform = "scale(.8)";
 
                 setTimeout(() => {
-
                     card.style.display = "none";
+                }, 300);
 
-                },300);
+            }
+
+        });
+
+
+        /* ==========================================
+           PROJECT BUTTONS
+        ========================================== */
+
+        projectButtons.forEach(buttonGroup => {
+
+            const category = buttonGroup.dataset.category;
+
+            if (filter === "all" || category === filter) {
+
+                buttonGroup.style.display = "flex";
+
+            } else {
+
+                buttonGroup.style.display = "none";
 
             }
 
